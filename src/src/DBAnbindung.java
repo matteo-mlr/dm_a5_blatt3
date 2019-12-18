0b6ce48153e7df9231f84220a0c1b7a082f954b7
Matteo <1824001@stud.hs-mannheim.de>
2019-12-18 13:58:07 +0100

    PreparedStatement

--- src/src/DBAnbindung.java
+++ src/src/DBAnbindung.java
@@ -131,6 +131,7 @@ public class DBAnbindung implements DBInterface {
 		int counter = 0;
 		
 		try {
+			
 			ps = con.prepareStatement(statement);
 			
 			for (int i = 0; i < argumente.length; i++) {
@@ -138,8 +139,6 @@ public class DBAnbindung implements DBInterface {
 				ps.setString((i+1), argumente[i]);
 				
 			}
-			
-			System.out.println(ps.toString());
 			
 			ResultSet rs = ps.executeQuery();
 			ResultSetMetaData rsmd = rs.getMetaData();
